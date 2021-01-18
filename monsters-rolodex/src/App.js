c6e85ee7a8d025a2574b84252015659e03910116
heyjessiehey <jessieko608@gmail.com>
2021-01-18 06:23:06 -0500

    added search box component

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor(){
     super();
     this.state= {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
   }
 
@@ -16,12 +19,17 @@ class App extends Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+        monster.name.toLowerCase().includes(searchField.toLowerCase())
+      );
     return (
       <div className="App">
-        {
-          this.state.monsters.map(monster => 
-            <h1 key={monster.id}>{monster.id} {monster.name}</h1>)
-        }
+        <SearchBox 
+          placeholder = 'Search Monsters'
+          handleChange = {e => this.setState({searchField: e.target.value})}
+        ></SearchBox>
+        <CardList monsters={filteredMonsters}></CardList>
       </div>
     );
   }
