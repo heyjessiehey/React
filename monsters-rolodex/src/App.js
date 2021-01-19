ba525c5d2da601cbc21cafcaf8f9d76135717780
heyjessiehey <jessieko608@gmail.com>
2021-01-19 05:35:00 -0500

    added handleChange method

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -18,6 +18,10 @@ class App extends Component {
       .then(users => this.setState({monsters: users}));
   }
 
+  handleChange = e =>{
+    this.setState({searchField: e.target.value})
+  }
+
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter(monster => 
@@ -27,7 +31,7 @@ class App extends Component {
       <div className="App">
         <SearchBox 
           placeholder = 'Search Monsters'
-          handleChange = {e => this.setState({searchField: e.target.value})}
+          handleChange = {this.handleChange}
         ></SearchBox>
         <CardList monsters={filteredMonsters}></CardList>
       </div>
